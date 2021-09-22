20105b5e1c52d0f1db198d13ad88c3fce67582ce
Michail Novos <mihasik323@gmail.com>
2021-09-22 13:16:47 +0300

    Added react-bootsrap instead of materialui, fixed eslint problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import React from 'react';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Col, Container, Row } from 'react-bootstrap';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello from react</h1>
+      <Container fluid>
+        <Row>
+          <Col>1 of 1</Col>
+        </Row>
+      </Container>
     </div>
   );
 }
